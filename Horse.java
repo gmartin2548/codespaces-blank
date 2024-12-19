e23159d7c3de0dd2a15c849a25feb3a24f9e3f20
gmartin2548 <gmart0711@gmail.com>
2024-12-19 14:46:59 +0000

    first commit

---
+++ Horse.java
@@ -0,0 +1,18 @@
+public class HorseRace{
+    private int speed;  //How many columns traveled 'per move'
+    private int lane;  //What row horse is in 
+    private int horseNum; //1-99
+    private static int distance;  //How many columns in raceArray
+    private String[][] raceArray;
+
+    //constructors
+    public Horse(){
+
+    }
+
+    public Horse(){
+
+    }
+    //methods
+    
+}
